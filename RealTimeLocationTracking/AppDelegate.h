f3d214e5700a18349ce06a2251a99ed9ca7997e7
Hemlata <hemlatakhajanchi@gmail.com>
2017-02-14 09:38:10 +0530

    First Commit.

---
+++ RealTimeLocationTracking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RealTimeLocationTracking
+//
+//  Created by Hema on 14/02/17.
+//  Copyright © 2017 Ranosys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RealTimeLocationTracking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RealTimeLocationTracking
+//
+//  Created by Hema on 14/02/17.
+//  Copyright © 2017 Ranosys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
